01962726fd30f09af51bf9908a8982c4823a9ec9
maelhemon <mael.hemon05@gmail.com>
2025-05-22 14:15:26 +0200

    add retr_cp

--- ville_vf.js
+++ ville_vf.js
@@ -4,18 +4,30 @@ var menu=document.getElementById('communes');
 var input_cp=document.getElementById('cp');
 function retr_cp(){
     //effacer le texte occurant
-    result.textContent="";
-    try{
+     result.textContent="";
+     let code_postal=input_cp.value
+     console.log(code_postal);
+    // tester le code postal avec une regex
+    const regex_cp=/[0-9][1-8]([0-9]{3})/;
+    if (regex_cp.test(code_postal)){
+         try{
          // faire la requête api
         //convertir l'api en json
         // insérer les objets dans le sélecte
-        let code_postal=input_cp.value
-        const response_api=fetch(`https://geo.api.gouv.fr/communes?codePostal=${code_postal}`).then(response => {response.json(); console.log(response)})
-        response_api.forEach( object_api => {
+        fetch(`https://geo.api.gouv.fr/communes?codePostal=${code_postal}`)
+        .then(response => response.json())
+        .then(data => {
+            menu.innerHTML="";
+            data.forEach( object_api => { // parcourt le tableau json composé de plusieurs object
+                console.log(object_api) // affiche chaque objet
             let commune=document.createElement('option');
-            commune.textcontent=object_api.nom ;
+            commune.textContent=object_api.nom ;
             commune.value=object_api.code;
             menu.appendChild(commune);
+        }
+
+        )
+        
         });
     }
     // gérer les erreurs
@@ -23,35 +35,41 @@ function retr_cp(){
         result.textContent="Erreur de l'api"  ;
     }
    
+    }
+    else{
+        result.textContent="Veuillez sélectionner un code postal valide !"
+    }
+   
 }
 function disp_weather(){
     result.textContent="";
-    //
-
+    // récupérer le nom de la commune
+    // requête api 
+    // afficher dans le dom les éléments demandés
+    //gestion des erreurs d'api
+    try{
+        fetch(fetch(`https://api.openweathermap.org/data/2.5/weather?q=${commune},FR&units=metric&lang=fr&appid=${apiKey}`))
+    }
+    catch(error){
+        result.textContent="Erreur de l'API"
+    }
 }
 
 function reset(){
     // actualiser la page
      window.location.reload();
 }
-document.getElementById('cp').addEventListener('input',async function() {
-     // tester le code postal avec une regex
-    let code_postal=input_cp.value;
-    console.log(code_postal);
-    const regex_cp=/[0-9][1-8]([0-9]{3})/;
-    if (regex_cp.test(code_postal)){
-        retr_cp();
-    }
-    else{
-        result.textContent="Veuillez sélectionner un code postal valide !"
-    }
+document.getElementById('cp').addEventListener('input',retr_cp);
     
-});
+    
+   
+    
+
 document.getElementById('retr_weather').addEventListener('click',async function() {
         
         let   insee=document.getElementById('commune').value;
-        if(!insee){
-            disp_weather();
+        if(insee!=null){
+            disp_weather(insee);
         }
         else{
             result.textcontent="Veuillez sélectionner une commune !";
