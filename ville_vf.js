4ac3b6a5e1c349d790108dc4191a987499958be0
maelhemon <mael.hemon05@gmail.com>
2025-05-22 13:06:36 +0200

    init nouvelle version du projet

--- ville_vf.js
+++ ville_vf.js
@@ -1,31 +1,61 @@
-document.getElementById("cp").addEventListener("input",async function() {
-    let code_postal = document.getElementById("cp").value;//récupère le code postal que l'utilisateur rentre
-    console.log(code_postal)
-    let select=document.getElementById("commune")
-    select.innerHTML='<option value="">-- Sélectionnez une commune --</option>'; //vide le menu déroulant à chaque reload de la page
-    fetch('https://geo.api.gouv.fr/communes?codePostal=' + code_postal) //requete get vers l'api
-        .then((response) => response.json()) // conversion du resultat de la requete en json
-        .then((json) => {  // le resultat se transforme en variable json
-            console.log("Réponse API :", json); // affiche dans la console la réponse de l'api
-           let data=json.map(commune=>`<option value="${commune.nom}">${commune.nom}</option>`).join(" ") // déclare une variable data qui va mapper chaque nom des communes présentes dans le json de l'api  via le .join
-           select.innerHTML+=data // l'afficher dans le menu déroulant
-           let insee=json.map(insee=>insee.code) // récupérer dans un tableau via une fonction flêchée et la méthode .map et .code qui permet de récupérer les valeurs du champs code insee de la réponse de l' API
-            console.log(insee)
-            return insee
-        })
-        .catch((error) => {
-            console.error("Erreur API :", error); // affiche dans la console le fail de l'interrogation de l'api
+// recupérer le dom
+var result=document.getElementById('result');
+var menu=document.getElementById('communes');
+var input_cp=document.getElementById('cp');
+function retr_cp(){
+    //effacer le texte occurant
+    result.textContent="";
+    try{
+         // faire la requête api
+        //convertir l'api en json
+        // insérer les objets dans le sélecte
+        let code_postal=input_cp.value
+        const response_api=fetch(`https://geo.api.gouv.fr/communes?codePostal=${code_postal}`).then(response => {response.json(); console.log(response)})
+        response_api.forEach( object_api => {
+            let commune=document.createElement('option');
+            commune.textcontent=object_api.nom ;
+            commune.value=object_api.code;
+            menu.appendChild(commune);
         });
-});
-//var listener_disp=document.getElementById("retr_meteo").addEventListener(InputEvent);
-//if (insee!=0 && listener_disp){
-//    let data = retr_post
-//    function retr_meteo(data){
-//
-//    }
-//}
+    }
+    // gérer les erreurs
+    catch(error){
+        result.textContent="Erreur de l'api"  ;
+    }
+   
+}
+function disp_weather(){
+    result.textContent="";
+    //
 
-// ajouter des eventlistener pour quand l'utilisateur rentre un cp la première api affiche les communes
-// ensuite lorsque l'utilisateur clique sur le bouton afficher => la météo s'affiche
-// faire une fonction qui permet d'effacer la recherche si erreur api ouvsi boutton effacer appuyer => window.location.reload();
-// faire en sorte que ce soit bien un code postal valide qui est rentré
+}
+
+function reset(){
+    // actualiser la page
+     window.location.reload();
+}
+document.getElementById('cp').addEventListener('input',async function() {
+     // tester le code postal avec une regex
+    let code_postal=input_cp.value;
+    console.log(code_postal);
+    const regex_cp=/[0-9][1-8]([0-9]{3})/;
+    if (regex_cp.test(code_postal)){
+        retr_cp();
+    }
+    else{
+        result.textContent="Veuillez sélectionner un code postal valide !"
+    }
+    
+});
+document.getElementById('retr_weather').addEventListener('click',async function() {
+        
+        let   insee=document.getElementById('commune').value;
+        if(!insee){
+            disp_weather();
+        }
+        else{
+            result.textcontent="Veuillez sélectionner une commune !";
+        }
+});
+document.getElementById('reset').addEventListener('click',reset );
+//tester si ya un code insee dans le select
